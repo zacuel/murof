3de78893cc28f607c671252a7592dedd02a7f236
Sam Zaccone <samrzaccone@gmail.com>
2024-07-03 12:56:24 -0400

    get dependencies firebase, flutter_riverpod.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:murof/firebase_tools/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.web);
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
